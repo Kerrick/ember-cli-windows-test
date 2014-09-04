99679d3f70610b459d593286f2c80f103434b289
Kerrick Long <kerrick@secondstreet.com>
2014-09-04 10:49:14 -0500

    Import a function from the addon's namespace

---
+++ addon/addon/utils/return-true.js
@@ -0,0 +1,3 @@
+export default function returnTrue() {
+  return true;
+}
--- addon/app/controllers/index.js
+++ addon/app/controllers/index.js
@@ -1,5 +1,7 @@
 import Ember from 'ember';
+import returnTrue from 'addon/utils/return-true';
 
 export default Ember.Controller.extend({
-  isControllerFromAddon: true
+  isControllerFromAddon: true,
+  isFunctionFromAddonNamespace: returnTrue()
 });
